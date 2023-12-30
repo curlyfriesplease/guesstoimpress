cafaaa58073fad4aee39b815cd5542951a0ee81f
curlyfriesplease <jmulhern85@gmail.com>
2023-12-30 17:29:48 +0000

    Adds 2024 rules, corrects spacing of guess extra info

--- src/components/guesses.js
+++ src/components/guesses.js
@@ -1,7 +1,10 @@
-import react from 'react';
 import '../css/guesses.css';
 import '../css/categories.css';
-import { fetchAllPredictionsByStatus, fetchAllPredictionsByAuthor, fetchAllPredictionsByCategory } from './queries';
+import {
+  fetchAllPredictionsByStatus,
+  fetchAllPredictionsByAuthor,
+  fetchAllPredictionsByCategory,
+} from './queries';
 
 export const ListDiv = ({ predictionData, setPredictions }) => {
   console.log(typeof predictionData);
@@ -16,22 +19,48 @@ export const ListDiv = ({ predictionData, setPredictions }) => {
   );
 };
 
-const StatusPending = ({setPredictions}) => <div className="statusPending status"  onClick={() => fetchAllPredictionsByStatus('1', setPredictions)}>PENDING</div>;
-const StatusIncorrect = ({setPredictions}) => (
-  <div className="statusIncorrect status" onClick={() => fetchAllPredictionsByStatus('2', setPredictions)}>INCORRECT</div>
+const StatusPending = ({ setPredictions }) => (
+  <div
+    className="statusPending status"
+    onClick={() => fetchAllPredictionsByStatus('1', setPredictions)}
+  >
+    PENDING
+  </div>
 );
-const StatusPartial = ({setPredictions}) => <div className="statusPartial status" onClick={() => fetchAllPredictionsByStatus('3', setPredictions)}>PARTIAL</div>;
-const StatusCorrect = ({setPredictions}) => <div className="statusCorrect status" onClick={() => fetchAllPredictionsByStatus('4', setPredictions)}>CORRECT</div>;
+const StatusIncorrect = ({ setPredictions }) => (
+  <div
+    className="statusIncorrect status"
+    onClick={() => fetchAllPredictionsByStatus('2', setPredictions)}
+  >
+    INCORRECT
+  </div>
+);
+const StatusPartial = ({ setPredictions }) => (
+  <div
+    className="statusPartial status"
+    onClick={() => fetchAllPredictionsByStatus('3', setPredictions)}
+  >
+    PARTIAL
+  </div>
+);
+const StatusCorrect = ({ setPredictions }) => (
+  <div
+    className="statusCorrect status"
+    onClick={() => fetchAllPredictionsByStatus('4', setPredictions)}
+  >
+    CORRECT
+  </div>
+);
 
 const IndividualGuess = ({ guess, setPredictions }) => {
   const statusText = (num) => {
     switch (num) {
-      case "1":
-        return <StatusPending  setPredictions={setPredictions}/>;
-      case "2":
-        return <StatusIncorrect setPredictions={setPredictions}/>;
-      case "3":
-        return <StatusPartial  setPredictions={setPredictions}/>;
+      case '1':
+        return <StatusPending setPredictions={setPredictions} />;
+      case '2':
+        return <StatusIncorrect setPredictions={setPredictions} />;
+      case '3':
+        return <StatusPartial setPredictions={setPredictions} />;
       default:
         return <StatusCorrect setPredictions={setPredictions} />;
     }
@@ -41,61 +70,113 @@ const IndividualGuess = ({ guess, setPredictions }) => {
     switch (cat) {
       case 'DEATH':
         return (
-          <div className="category DEATH" onClick={() => fetchAllPredictionsByCategory(guess.category, setPredictions)}>
+          <div
+            className="category DEATH"
+            onClick={() =>
+              fetchAllPredictionsByCategory(guess.category, setPredictions)
+            }
+          >
             <span className="material-icons-outlined">person_remove</span> {cat}
           </div>
         );
       case 'ECONOMY':
         return (
-          <div className="category ECONOMY" onClick={() => fetchAllPredictionsByCategory(guess.category, setPredictions)}>
+          <div
+            className="category ECONOMY"
+            onClick={() =>
+              fetchAllPredictionsByCategory(guess.category, setPredictions)
+            }
+          >
             <span className="material-icons-outlined">trending_up</span> {cat}
           </div>
         );
       case 'ENVIRONMENT':
         return (
-          <div className="category ENVIRONMENT" onClick={() => fetchAllPredictionsByCategory(guess.category, setPredictions)}>
+          <div
+            className="category ENVIRONMENT"
+            onClick={() =>
+              fetchAllPredictionsByCategory(guess.category, setPredictions)
+            }
+          >
             <span className="material-icons-outlined">air</span> {cat}
           </div>
         );
       case 'POLITICS':
         return (
-          <div className="category POLITICS" onClick={() => fetchAllPredictionsByCategory(guess.category, setPredictions)}>
-            <span className="material-icons-outlined">sports_kabaddi</span> {cat}
+          <div
+            className="category POLITICS"
+            onClick={() =>
+              fetchAllPredictionsByCategory(guess.category, setPredictions)
+            }
+          >
+            <span className="material-icons-outlined">sports_kabaddi</span>{' '}
+            {cat}
           </div>
         );
       case 'SPORTS':
         return (
-          <div className="category SPORTS" onClick={() => fetchAllPredictionsByCategory(guess.category, setPredictions)}>
-            <span className="material-icons-outlined">sports_basketball</span> {cat}
+          <div
+            className="category SPORTS"
+            onClick={() =>
+              fetchAllPredictionsByCategory(guess.category, setPredictions)
+            }
+          >
+            <span className="material-icons-outlined">sports_basketball</span>{' '}
+            {cat}
           </div>
         );
       case 'MEDIA':
         return (
-          <div className="category MEDIA" onClick={() => fetchAllPredictionsByCategory(guess.category, setPredictions)}>
+          <div
+            className="category MEDIA"
+            onClick={() =>
+              fetchAllPredictionsByCategory(guess.category, setPredictions)
+            }
+          >
             <span className="material-icons-outlined">tv</span> {cat}
           </div>
         );
       case 'SCIENCE':
         return (
-          <div className="category SCIENCE" onClick={() => fetchAllPredictionsByCategory(guess.category, setPredictions)}>
+          <div
+            className="category SCIENCE"
+            onClick={() =>
+              fetchAllPredictionsByCategory(guess.category, setPredictions)
+            }
+          >
             <span className="material-icons-outlined">science</span> {cat}
           </div>
         );
       case 'CELEBRITY':
         return (
-          <div className="category CELEBRITY" onClick={() => fetchAllPredictionsByCategory(guess.category, setPredictions)}>
+          <div
+            className="category CELEBRITY"
+            onClick={() =>
+              fetchAllPredictionsByCategory(guess.category, setPredictions)
+            }
+          >
             <span className="material-icons-outlined">photo_camera</span> {cat}
           </div>
         );
       case 'VIRUS':
         return (
-          <div className="category VIRUS" onClick={() => fetchAllPredictionsByCategory(guess.category, setPredictions)}>
+          <div
+            className="category VIRUS"
+            onClick={() =>
+              fetchAllPredictionsByCategory(guess.category, setPredictions)
+            }
+          >
             <span className="material-icons-outlined">coronavirus</span> {cat}
           </div>
         );
       default:
         return (
-          <div className="category MISC" onClick={() => fetchAllPredictionsByCategory(guess.category, setPredictions)}>
+          <div
+            className="category MISC"
+            onClick={() =>
+              fetchAllPredictionsByCategory(guess.category, setPredictions)
+            }
+          >
             <span className="material-icons-outlined">grade</span> {cat}
           </div>
         );
@@ -105,12 +186,20 @@ const IndividualGuess = ({ guess, setPredictions }) => {
   return (
     <div className="individualGuess dropShadow2">
       <div className="guessCategory">{categoryText(guess.category)}</div>
-      <div className="guessAuthor" onClick={() => fetchAllPredictionsByAuthor(guess.Author, setPredictions)}>{guess.Author}</div>
-      <div className="guessText"> {guess.Prediction}</div>
+      <div
+        className="guessAuthor"
+        onClick={() =>
+          fetchAllPredictionsByAuthor(guess.Author, setPredictions)
+        }
+      >
+        {guess.Author}
+      </div>
+      <div className="guessText">
+        <div>{guess.Prediction}</div>
+        {guess.info && <div className="guessInfo">{guess.info} </div>}
+      </div>
       <div className="guessYear">{guess.Year}</div>
       <div className="guessStatus">{statusText(guess.status)}</div>
-      <div className="guessInfo">{guess.info} </div>
     </div>
   );
 };
-
--- src/views/rules.js
+++ src/views/rules.js
@@ -1,24 +1,63 @@
-import React from "react";
-import "../css/rules.css";
-import "../App.css";
-import {
-  YearContainer,
-} from "../components/winners";
+import React from 'react';
+import '../css/rules.css';
+import '../App.css';
+import { YearContainer } from '../components/winners';
 
 export const Rules = () => (
   <>
-    <YearContainer year={"2023"}>
+    <YearContainer year={'2024'}>
+      <p className="addedRule">
+        Added: To keep things interesting, rule added to limit boring guesses
+      </p>
+      <p className="addedRule">
+        Added: Deadline extended to Jan 4th, to allow for hangovers and
+        dilly-dallyers
+      </p>
+      <p className="addedRule">
+        Added: No celeb death guesses for anyone known to be ill
+      </p>
+      <p className="removedRule">
+        Removed: Political guess no longer essential
+      </p>
+      <ol className="annualRulesList">
+        <li>
+          Up to <u>eight</u> predictions in total
+        </li>
+        <li>
+          No boring as shit guesses. A non-player, the Boringness Ombusman, will
+          be chosen and asked to read through all guesses at the start of the
+          year. Anything deemed to be dull will be removed
+        </li>
+        <li>Any celebrity deaths must be aged under 85 when the year begins</li>
+        <li>Any celebrity deaths must not be for anyone known to be ill</li>
+        <li>
+          No predictions within your control, e.g. "I will run a marathon this
+          year"
+        </li>
+        <li>Wildest set of predictions will win a prize</li>
+        <li>
+          Most impressive individual prediction, that is at least partly
+          correct, wins a prize
+        </li>
+        <li>
+          Prizes: Best individual prediction wins 3 books, chosen by the
+          previous year's winners. Wildest set of predictions, and most
+          creative, get a pint. Or more than that if it's close to payday when I
+          see you.
+        </li>
+        <li>Predictions must be submitted by the end of Jan 4th</li>
+      </ol>
+    </YearContainer>
+    <YearContainer year={'2023'}>
       <p className="removedRule">
         Removed: Celebrity death being essential. Still strongly encouraged but
         no longer essential
       </p>
       <ol className="annualRulesList">
-      <li>
+        <li>
           Up to <u>eight</u> predictions in total
         </li>
-        <li>
-          Any celebrity deaths must be aged under 85 when the year begins
-        </li>
+        <li>Any celebrity deaths must be aged under 85 when the year begins</li>
         <li>At least one political prediction</li>
         <li>
           No predictions within your control, e.g. "I will run a marathon this
@@ -30,14 +69,15 @@ export const Rules = () => (
           correct, wins a prize
         </li>
         <li>
-          Prizes to be determined at the end of the year. Depends what I can afford!
+          Prizes: Best individual prediction wins 3 books, chosen by the
+          previous year's winners. Wildest set of predictions, and most
+          creative, get a pint. Or more than that if it's close to payday when I
+          see you.
         </li>
-        <li>
-          Predictions must be submitted by the end of Jan 1st
-        </li>
+        <li>Predictions must be submitted by the end of Jan 1st</li>
       </ol>
     </YearContainer>
-    <YearContainer year={"2022"}>
+    <YearContainer year={'2022'}>
       <p className="addedRule">
         Added: An additional three guesses, up to eight total, to attract more
         wild and unusual predictions
@@ -50,7 +90,9 @@ export const Rules = () => (
         Added: A rule that it must be something out of your control
       </p>
       <p className="removedRule">
-        Removed: Deciding what the prizes are before the year starts. Seemed like a good idea at the time to say there was a polaroid camera at the other end. But when the time came around, my wallet said baby please no 
+        Removed: Deciding what the prizes are before the year starts. Seemed
+        like a good idea at the time to say there was a polaroid camera at the
+        other end. But when the time came around, my wallet said baby please no
       </p>
       <ol className="annualRulesList">
         <li>
@@ -70,14 +112,15 @@ export const Rules = () => (
           correct, wins a prize
         </li>
         <li>
-          Prizes to be determined at the end of the year depending on whether I've saved any money or not. Probably not. So just a pint each most likely.
+          Prizes: Best individual prediction wins 3 books, chosen by the
+          previous year's winners. Wildest set of predictions, and most
+          creative, get a pint. Or more than that if it's close to payday when I
+          see you.
         </li>
-        <li>
-          Predictions must be submitted by the end of Jan 1st
-        </li>
+        <li>Predictions must be submitted by the end of Jan 1st</li>
       </ol>
     </YearContainer>
-    <YearContainer year={"2021"}>
+    <YearContainer year={'2021'}>
       <ol className="annualRulesList">
         <li>
           Up to <u>five</u> predictions in total
