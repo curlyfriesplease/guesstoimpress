a1a63df6d0966dc0c1127c3d0b88f0bbd37246d6
curlyfriesplease <jmulhern85@gmail.com>
2024-01-04 22:15:58 +0000

    Corrects clipping of title

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,5 @@
-import { redirect, Link } from 'react-router-dom';
 import '../css/header.css';
-import { fetchAllPredictionsByYear } from './queries';
-import { useNavigate } from "react-router-dom";
-
-
-// const handleClick = (year, setPredictions) => {
-//   redirect('/error');
-//   fetchAllPredictionsByYear(year, setPredictions);
-// };
+import { useNavigate } from 'react-router-dom';
 
 export const Header = ({ setPredictions }) => (
   <div className="headerDiv dropShadow2">
@@ -19,19 +11,20 @@ export const Header = ({ setPredictions }) => (
 const Title = () => <div className="title">Guess to Impress</div>;
 
 const MenuItems = ({ setPredictions }) => {
-const navigate = useNavigate();
-return (
-  <>
-    <div className="menuItems">
-      <button className="menuButton" onClick={() => navigate('/')}>
-        HOME
-      </button>
-      <button className="menuButton"onClick={() => navigate('/rules')}>
-        RULES
-      </button>
-      <button className="menuButton"onClick={() => navigate('/winners')}>
-        WINNERS
-      </button>
-    </div>
-  </>
-)};
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="menuItems">
+        <button className="menuButton" onClick={() => navigate('/')}>
+          HOME
+        </button>
+        <button className="menuButton" onClick={() => navigate('/rules')}>
+          RULES
+        </button>
+        <button className="menuButton" onClick={() => navigate('/winners')}>
+          WINNERS
+        </button>
+      </div>
+    </>
+  );
+};
--- src/views/yearpage.js
+++ src/views/yearpage.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { ListDiv } from '../components/guesses';
 import {
-  fetchAllPredictionsAndIMeanAllOfThem,
   fetchAllPredictionsByYear,
   fetchAllPredictionsByYearWithoutIncorrects,
 } from '../components/queries';
